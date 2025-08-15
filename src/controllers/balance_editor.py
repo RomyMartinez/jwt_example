573c83c7937dc361ce06f92f781a590bac4b6a21
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-14 23:40:13 -0300

    feat: Implementing Balance Editor

---
+++ src/controllers/balance_editor.py
@@ -0,0 +1,15 @@
+from ast import Dict
+from src.models.interfaces.user_repository import UserRepositoryInterface
+
+class BalanceEditor:
+    def __init__(self, user_repository: UserRepositoryInterface) -> None:
+        self.__user_repository = user_repository
+
+    def edit_balance(self, user_id: str, new_balance: float) -> Dict:
+        self.__user_repository.edit_balance(user_id, new_balance)
+
+        return {
+            "type": "User",
+            "count": 1,
+            "new balance": new_balance,
+        }
